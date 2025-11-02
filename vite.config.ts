3c85c5a2687b5afb69a33a3738b613c332e05492
Gaia Cecchi <gaia.cecchi17@gmail.com>
2025-11-02 17:04:26 +0100

    Update base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/PKMN-TCGP-PromoA/',
+  base: '/Gaia-Cecchi-PKMN-TCGP-PromoA/',
 })
